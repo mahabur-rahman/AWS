49924c12dfd17ac07fb6ad0f4f62f2279f5a84b3
mahabur-rahman <annur4395@gmail.com>
2024-07-31 04:12:31 +0600

    feat: cdk deoploy

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -6,11 +6,5 @@ export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
