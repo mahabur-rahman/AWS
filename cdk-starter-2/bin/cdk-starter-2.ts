f712986e9f5d93ebfbcf2849e2dd780a8ac6a27c
mahabur-rahman <annur4395@gmail.com>
2024-08-17 12:34:27 +0600

    tagger

--- cdk-starter-2/bin/cdk-starter-2.ts
+++ cdk-starter-2/bin/cdk-starter-2.ts
@@ -4,9 +4,13 @@ import * as cdk from 'aws-cdk-lib';
 // import { CdkStarter2Stack } from '../lib/cdk-starter-2-stack';
 import { PhotosStack } from '../lib/PhotoStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
     targetBucketArn: photosStack.photosBucketArn
 }); 
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
