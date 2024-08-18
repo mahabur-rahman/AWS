b0449309be97279ded61c935516417d390ca7157
mahabur-rahman <annur4395@gmail.com>
2024-08-18 13:13:07 +0600

    fix: fix issue on LambdaFunc

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,11 +1,12 @@
+// LambdaStack.ts
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Code, Runtime, Function as LambdaFunction } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import { Construct } from "constructs";
 
-
-export class LambdaStack extends Stack{
-    constructor(scope: Stack, id: string, props?: StackProps) {
-        super(scope, id);
+export class LambdaStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
         new LambdaFunction(this, 'HelloLambda', {
             runtime: Runtime.NODEJS_18_X,
