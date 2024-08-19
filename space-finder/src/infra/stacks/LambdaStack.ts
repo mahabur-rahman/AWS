3948f1a48d547bb1a3db21f7164855f8296a33e1
mahabur-rahman <annur4395@gmail.com>
2024-08-19 13:08:57 +0600

    feat: API gateway with lambda func

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,17 +1,26 @@
 // LambdaStack.ts
 import { Stack, StackProps } from "aws-cdk-lib";
-import { Code, Runtime, Function as LambdaFunction } from "aws-cdk-lib/aws-lambda";
+import {
+  Code,
+  Runtime,
+  Function as LambdaFunction,
+} from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
 import { Construct } from "constructs";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 
 export class LambdaStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  public readonly helloLambdaIntegration: LambdaIntegration;
+  
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        new LambdaFunction(this, 'HelloLambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: 'hello.main',
-            code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
-        });
-    }
+    const helloLambda = new LambdaFunction(this, "HelloLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "hello.main",
+      code: Code.fromAsset(join(__dirname, "..", "..", "services")),
+    });
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
+  }
 }
