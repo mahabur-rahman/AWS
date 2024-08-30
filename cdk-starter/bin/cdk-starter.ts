a6f92338f85c780a4f12eab1809185d65738bc82
mahabur-rahman <annur4395@gmail.com>
2024-08-30 16:06:05 +0600

    remove depenedy

--- cdk-starter/bin/cdk-starter.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkStarterStack } from '../lib/cdk-starter-stack';
-
-const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack');
--- space-finder/src/services/hello.js
+++
@@ -1,6 +0,0 @@
-exports.main = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`Hello!, i will read from ${process.env.TABLE_NAME}`),
-  };
-};
