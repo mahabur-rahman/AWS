89f6ab7df024233951116080ceec52bc16463ea2
mahabur-rahman <annur4395@gmail.com>
2024-09-20 17:10:14 +0600

    Feat: SignIn and signOut feature with amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import Test from "./components/Test";
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports'; // No more errors with this import
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
 
-export default function App() {
-  return (
-    <>
-      <Test />
-    </>
-  );
+Amplify.configure(awsconfig);
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Authenticator>
+                    {({ signOut, user }) => (
+                        <main>
+                            <h2>Welcome, {user?.userId}!</h2>
+                            <button onClick={signOut}>Sign Out</button>
+                        </main>
+                    )}
+                </Authenticator>
+            </header>
+        </div>
+    );
 }
+
+export default withAuthenticator(App);
+
